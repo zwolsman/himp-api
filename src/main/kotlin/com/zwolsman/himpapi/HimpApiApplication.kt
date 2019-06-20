1b1c9b59d5ff74cea089124304f88ba84de5811d
Marvin Zwolsman <mzwolsman@bol.com>
2019-06-20 10:49:50 +0200

    Initial commit

---
+++ src/main/kotlin/com/zwolsman/himpapi/HimpApiApplication.kt
@@ -0,0 +1,11 @@
+package com.zwolsman.himpapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HimpApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HimpApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/zwolsman/himpapi/HimpApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zwolsman.himpapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HimpApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
